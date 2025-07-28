fa5fb36d10122c20ac6e1b88ef0a43eb66191602
Madhusha Prasad <madushaprasad21@gmail.com>
2025-07-28 17:50:03 +0530

    feat: add dependencies

--- src/main/java/com/example/pos/configs/WebRootConfig.java
+++ src/main/java/com/example/pos/configs/WebRootConfig.java
@@ -1,7 +1,9 @@
 package com.example.pos.configs;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import({JPAConfig.class})
 public class WebRootConfig {
 }
