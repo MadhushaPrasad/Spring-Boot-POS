36ac053bfc1c5bc88aa1802abb8d0b642f6c472e
Madhusha Prasad <madushaprasad21@gmail.com>
2025-07-28 19:10:42 +0530

    feat: create customer all crud in service

--- src/main/java/com/example/pos/services/CustomerService.java
+++ src/main/java/com/example/pos/services/CustomerService.java
@@ -2,6 +2,16 @@ package com.example.pos.services;
 
 import com.example.pos.entity.Customer;
 
+import java.util.List;
+
 public interface CustomerService {
     void saveCustomer(Customer customer);
+
+    void updateCustomer(Customer customer);
+
+    void deleteCustomer(String id);
+
+    Customer searchCustomer(String id);
+
+    List<Customer> getAllCustomer();
 }
--- src/main/java/com/example/pos/services/impl/CustomerServiceImpl.java
+++ src/main/java/com/example/pos/services/impl/CustomerServiceImpl.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+
 
 @Service
 @Transactional
@@ -18,5 +21,28 @@ public class CustomerServiceImpl implements CustomerService {
     @Override
     public void saveCustomer(Customer customer) {
         Customer savedCustomer = customerRepository.save(customer);
+    }
+
+    @Override
+    public void updateCustomer(Customer customer) {
+        Customer updatedCustomer = customerRepository.save(customer);
+    }
+
+    @Override
+    public void deleteCustomer(String id) {
+        Optional<Customer> searchedCustomer = customerRepository.findById(id);
+        customerRepository.delete(searchedCustomer.get() );
+    }
+
+    @Override
+    public Customer searchCustomer(String id) {
+        Optional<Customer> searchedCustomer = customerRepository.findById(id);
+
+        return searchedCustomer.get();
+    }
+
+    @Override
+    public List<Customer> getAllCustomer() {
+        return customerRepository.findAll();
     }
 }
