5432adc0ab2ee224be5992c18b48dc3e6e64f792
Madhusha Prasad <madushaprasad21@gmail.com>
2025-07-29 11:56:45 +0530

    chore: change response to response entity

--- src/main/java/com/example/pos/adviser/AppWideAdviser.java
+++ src/main/java/com/example/pos/adviser/AppWideAdviser.java
@@ -1,5 +1,8 @@
 package com.example.pos.adviser;
 
+import com.example.pos.handler.AppException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -7,7 +10,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class AppWideAdviser {
 
     @ExceptionHandler(Exception.class)
-    public String handleMyException(Exception ex) {
+    public ResponseEntity handleMyException(Exception ex) {
+        return new ResponseEntity(ex.getMessage(), HttpStatus.OK);
+    }
+
+    @ExceptionHandler(AppException.class)
+    public String handleMyException(AppException ex) {
         return ex.getMessage();
     }
 }
--- src/main/java/com/example/pos/controller/CustomerController.java
+++ src/main/java/com/example/pos/controller/CustomerController.java
@@ -3,6 +3,8 @@ package com.example.pos.controller;
 import com.example.pos.dto.CustomerDTO;
 import com.example.pos.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,27 +18,32 @@ public class CustomerController {
     CustomerService customerService;
 
     @GetMapping
-    public List<CustomerDTO> getAllCustomer(){
-        return customerService.getAllCustomer();
+    public ResponseEntity getAllCustomer(){
+        List<CustomerDTO> allCustomer = customerService.getAllCustomer();
+        return new ResponseEntity(allCustomer, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public CustomerDTO searchCustomer(@PathVariable String id) {
-        return customerService.searchCustomer(id);
+    public ResponseEntity searchCustomer(@PathVariable String id) {
+        CustomerDTO customer = customerService.searchCustomer(id);
+        return new ResponseEntity(customer,HttpStatus.OK);
     }
 
     @PostMapping()
-    public void saveCustomer(@RequestBody CustomerDTO customer){
+    public ResponseEntity saveCustomer(@RequestBody CustomerDTO customer){
         customerService.saveCustomer(customer);
+        return new ResponseEntity("Done",HttpStatus.OK);
     }
 
     @DeleteMapping(params = {"id"})
-    public void deleteCustomer(@RequestParam String id){
+    public ResponseEntity deleteCustomer(@RequestParam String id){
         customerService.deleteCustomer(id);
+        return new ResponseEntity("Done",HttpStatus.OK);
     }
 
     @PutMapping
-    public void updateCustomer(@RequestBody CustomerDTO customer){
+    public ResponseEntity updateCustomer(@RequestBody CustomerDTO customer){
         customerService.updateCustomer(customer);
+        return new ResponseEntity("Done",HttpStatus.OK);
     }
 }
