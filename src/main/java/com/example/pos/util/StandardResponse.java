0fa2bdb84dd29204699fb09bd6aa1d62c8496b0e
Madhusha Prasad <madushaprasad21@gmail.com>
2025-07-30 11:53:15 +0530

    chore: fixed missing getters and setters of Standard response class

--- src/main/java/com/example/pos/util/StandardResponse.java
+++ src/main/java/com/example/pos/util/StandardResponse.java
@@ -21,4 +21,20 @@ public class StandardResponse {
     public void setCode(int code) {
         this.code = code;
     }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
 }
